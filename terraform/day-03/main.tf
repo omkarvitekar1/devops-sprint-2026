49ff2c8f84eaea4456cd4bb3316d0b6d065ecd43
DevOps Sprint <devops@Omkars-MacBook-Air.local>
2026-05-21 14:22:58 +0530

    Day4: Add lifecycle, prevent destroy protection to VPC

--- terraform/day-03/main.tf
+++ terraform/day-03/main.tf
@@ -1,12 +1,12 @@
 terraform {
-    required_version = ">= 1.5.0"
+  required_version = ">= 1.5.0"
 
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
 # Provider configured to point to LocalStack instead of AWS
@@ -19,7 +19,7 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    s3 = "http://localhost:4566"
+    s3  = "http://localhost:4566"
     ec2 = "http://localhost:4566"
     iam = "http://localhost:4566"
   }
@@ -27,21 +27,25 @@ provider "aws" {
 
 # VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
     Name = "sprint-day3-vpc"
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
 
 #Public Subnet
 
 resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "sprint-day3-public-subnet"
@@ -51,8 +55,8 @@ resource "aws_subnet" "public" {
 # Private Subnet
 
 resource "aws_subnet" "private" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
   tags = {
     Name = "sprint-day3-private-subnet"
@@ -82,6 +86,6 @@ resource "aws_route_table" "public" {
 
 # Associate Public Subnet with Public Route Table
 resource "aws_route_table_association" "public" {
-  subnet_id = aws_subnet.public.id
+  subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
