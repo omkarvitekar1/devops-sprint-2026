27ec3ae1fec78ce8193353c687250ecb38a59852
DevOps Sprint <devops@Omkars-MacBook-Air.local>
2026-05-21 11:40:29 +0530

    Day 3 : VPC + subnets + IGW + route table on Localstack

---
+++ terraform/day-03/main.tf
@@ -0,0 +1,87 @@
+terraform {
+    required_version = ">= 1.5.0"
+
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+      }
+    }
+}
+
+# Provider configured to point to LocalStack instead of AWS
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+    ec2 = "http://localhost:4566"
+    iam = "http://localhost:4566"
+  }
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags = {
+    Name = "sprint-day3-vpc"
+  }
+}
+
+#Public Subnet
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "sprint-day3-public-subnet"
+  }
+}
+
+# Private Subnet
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "sprint-day3-private-subnet"
+  }
+}
+
+# Internet Gateway
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "sprint-day3-igw"
+  }
+}
+
+# Public Route Table 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "sprint-day3-public-rt"
+  }
+}
+
+# Associate Public Subnet with Public Route Table
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
